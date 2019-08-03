202771a992b92365f7b8405e3416a80f80f207b6
Janik <janik.leddin@web.de>
2019-08-03 17:54:12 +0200

    fifty percent of the third boss

---
+++ Assets/Code/Boss3Control.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss3Control : MonoBehaviour
+{
+    public Transform Player;
+    public GameObject bossGameObject;
+    Rigidbody2D rbBoss3;
+    public float movementSpeed = 5;
+    public float hitPoints;
+    public float MaxDist = 100000;
+    public float MinDist = 0;
+
+    void Start()
+    {
+        hitPoints = 10;
+        rbBoss3 = GetComponent<Rigidbody2D>();
+    }
+
+     void Update()
+    {
+        transform.LookAt(Player);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            hitPoints--;
+            Debug.Log(hitPoints);
+            if (hitPoints == 0)
+            {
+                bossGameObject.SetActive(false);
+            }
+        }
+    }
+
+}
