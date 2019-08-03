b701770fd2cf360b23fb94ef32d9f909a6efa361
Janik <janik.leddin@web.de>
2019-08-03 13:36:50 +0200

    safe before bugfixing #3

---
+++ Assets/Scenes/Schussfaehigkeit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class Schussfaehigkeit : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    private void Update()
+    {
+        Instantiate (object, position, rotation);
+    }
+
+
+
+}
+
