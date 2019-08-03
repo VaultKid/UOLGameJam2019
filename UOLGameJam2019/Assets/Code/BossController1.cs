c92826d6529fc13210b0eb6bead881fdf981ae05
Vault Kid <avaultkid@gmail.com>
2019-08-03 12:17:36 +0200

    Erster Boss erstellt. Boss hat Hitpoints. Boss verliert Hitpoints wenn er Objekte berührt die mit "Bullet" getaggt sind #9

--- UOLGameJam2019/Assets/Code/BossController1.cs
+++ UOLGameJam2019/Assets/Code/BossController1.cs
@@ -5,18 +5,16 @@ using UnityEngine.AI;
 
 public class BossController1 : MonoBehaviour
 {
-
+    public GameObject bossGameObject;
     Rigidbody2D rbBoss;
-    Vector2 startPosition;
-    public float roamRadius;
-    //private NavMeshAgent navAgent;
+    public float speed;
+    float hitPoints;
 
     void Start()
     {
+        hitPoints = 5;
         rbBoss = GetComponent<Rigidbody2D>();
-        //navAgent = GetComponent<NavMeshAgent>();
-        startPosition = transform.position;
-        InvokeRepeating("randomBossMovement", 10.0f, 10.0f);
+        InvokeRepeating("randomBossMovement", 1f, 1.0f);
 
     }
     
@@ -26,18 +24,17 @@ public class BossController1 : MonoBehaviour
     }
 
     public void randomBossMovement() {
-
         Vector2 randomMovement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-        rbBoss.AddForce(randomMovement*roamRadius);
-        /*
-        Vector2 randomDirection = Random.insideUnitSphere;
-        Debug.Log(randomDirection);
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
-        Vector2 finalPosition = new Vector2(hit.position.x, hit.position.y);
-        //navAgent.destination = finalPosition;
-        */
+        rbBoss.AddForce(randomMovement*speed);
+    }
 
-
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Bullet")) {
+            hitPoints--;
+            Debug.Log(hitPoints);
+            if(hitPoints == 0) {
+                bossGameObject.SetActive(false);
+            }
+        }
     }
 }
