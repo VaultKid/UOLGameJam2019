07ba74deec1b0f3c639fb4a1cf53f9c6b467bcb2
Mark Otten <mark>
2019-08-03 18:19:22 +0200

    feature completted #6

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log(@"Quit Game.");
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
