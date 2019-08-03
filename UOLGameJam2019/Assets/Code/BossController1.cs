ec43f571471a3366ae2144ffca4fb86cb81e8cc7
Vault Kid <avaultkid@gmail.com>
2019-08-03 10:45:22 +0200

    whacky random player movement

---
+++ UOLGameJam2019/Assets/Code/BossController1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossController1 : MonoBehaviour
+{
+
+    Rigidbody2D rbBoss;
+    Vector2 startPosition;
+    public float roamRadius;
+    //private NavMeshAgent navAgent;
+
+    void Start()
+    {
+        rbBoss = GetComponent<Rigidbody2D>();
+        //navAgent = GetComponent<NavMeshAgent>();
+        startPosition = transform.position;
+        InvokeRepeating("randomBossMovement", 10.0f, 10.0f);
+
+    }
+    
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void randomBossMovement() {
+
+        Vector2 randomMovement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        rbBoss.AddForce(randomMovement*roamRadius);
+        /*
+        Vector2 randomDirection = Random.insideUnitSphere;
+        Debug.Log(randomDirection);
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
+        Vector2 finalPosition = new Vector2(hit.position.x, hit.position.y);
+        //navAgent.destination = finalPosition;
+        */
+
+
+    }
+}
