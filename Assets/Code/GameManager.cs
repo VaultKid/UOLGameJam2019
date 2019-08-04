a67ca0e08db5bc0a0549808b207f55849f0eed48
Vault Kid <avaultkid@gmail.com>
2019-08-04 12:41:16 +0200

    Spielerprogress und Powerups implementiert #31

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,8 +10,10 @@ public class GameManager : MonoBehaviour
     public GameObject overlay;
     private bool won;
     private bool allowContinue;
+    public int bossNumber;
+    public Text powerUpText;
 
-    public bool invisibility;
+    //private bool invisibility;
 
     private void Start() {
         won = false;
@@ -24,6 +27,15 @@ public class GameManager : MonoBehaviour
             Victory();
         }
         if (Input.GetButton("Fire1") && allowContinue) {
+            if(bossNumber == 1) {
+                FindObjectOfType<Progress>().invisibility = true;
+            }
+            if (bossNumber == 2) {
+                FindObjectOfType<Progress>().slowBullets = true;
+            }
+            if (bossNumber == 3) {
+                FindObjectOfType<Progress>().slowFireRate = true;
+            }
             SceneManager.LoadScene(1);
         }
     }
@@ -35,6 +47,16 @@ public class GameManager : MonoBehaviour
     IEnumerator wait() {
         yield return new WaitForSeconds(2.0f);
         overlay.SetActive(true);
+        if (bossNumber == 1) {
+            powerUpText.text = "Invisibility gained!";
+        }
+        if (bossNumber == 2) {
+            powerUpText.text = "More DMG but slower bullets gained!";
+        }
+        if (bossNumber == 3) {
+            powerUpText.text = "More DMG but slower firerate gained!";
+        }
+
         allowContinue = true;
     }
 }
